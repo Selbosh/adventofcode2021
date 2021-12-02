f5ea9f3847c102db3033d17bf9d0239d11951b50
Selby <david@selbys.org.uk>
2021-12-02 15:27:12 +0000

    Fix description of day 2 unit test

--- tests/testthat/test-day02.R
+++ tests/testthat/test-day02.R
@@ -1,4 +1,4 @@
-test_that("multiplication works", {
+test_that("Day 2", {
   example <- data.frame(cmd = c('forward', 'down', 'forward', 'up',
                                 'down', 'forward'),
                         value = c(5, 5, 8, 3, 8, 2))
