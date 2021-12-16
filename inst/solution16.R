9281203046fcd76b868167a79db84e507d9a4b01
Selby <david@selbys.org.uk>
2021-12-16 22:57:58 +0000

    Neater solution

--- inst/solution16.R
+++ inst/solution16.R
@@ -1,4 +1,4 @@
 library(adventofcode2021)
 input <- to_bits(readLines(input_file(16)))
 packet_versions(input)
-eval(str2expression(packet_parse(packet_decode(input))))
+packet_parse(packet_decode(input), T)
