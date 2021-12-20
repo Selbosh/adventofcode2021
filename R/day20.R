7e6f7243f8c9cab00fd255e2e6ad10e41b28a5c3
Selby <david@selbys.org.uk>
2021-12-20 13:14:33 +0000

    Add part 2 description

--- R/day20.R
+++ R/day20.R
@@ -104,6 +104,14 @@
 #'
 #' Start with the original input image and apply the image enhancement algorithm twice, being careful to account for the infinite size of the images. How many pixels are lit in the resulting image?
 #'
+#' ## Part Two:
+#'
+#' You still can't quite make out the details in the image. Maybe you just didn't enhance it enough.
+#'
+#' If you enhance the starting input image in the above example a total of 50 times, 3351 pixels are lit in the final output image.
+#'
+#' Start again with the original input image and apply the image enhancement algorithm 50 times. How many pixels are lit in the resulting image?
+#'
 #' @source <https://adventofcode.com/2021/day/20>
 #' @name day20
 NULL
