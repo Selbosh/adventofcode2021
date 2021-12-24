d8b745a640ae4ed3d3d8b3e4e2c23cd9a1b4b035
David Selby <david.selby@manchester.ac.uk>
2021-12-24 23:17:23 +0000

    Day 23 part 2

--- R/day23.R
+++ R/day23.R
@@ -8,9 +8,38 @@ NULL
 read_amphipod <- function(file) {
   mat <- read.matrix(file)
   hallways <- mat[2, setdiff(2:12, c(4, 6, 8, 10))]
-  rooms <- mat[cbind(rep(3:4, 4), c(4, 6, 8, 10))]
+  rooms <- mat[cbind(rep(3:4, 4), rep(c(4, 6, 8, 10), each = 2))]
   rooms <- setNames(rooms, paste0(rep(LETTERS[1:4], each = 2), 1:2))
-  list(rooms = rooms, hallways = hallways)
+  State$new(rooms, hallways)
+}
+
+#' @rdname day23
+#' @param start A `State` R6 object describing the initial position.
+#' @export
+min_amphipod_energy <- function(start) {
+  states <- list(start)
+  best_score <- Inf
+  while (length(states)) {
+    new_states <- list()
+    for (state in states) {
+      if (is.finite(best_score))
+        message('Best score: ', best_score)
+      for (move in valid_moves(state)) {
+        if (move$finished) {
+          best_score <- min(best_score, move$score)
+        } else if (move$score < best_score) {
+          if (move$key %in% names(new_states)) {
+            old_score <- new_states[[move$key]]$score
+            if (old_score > move$score)
+              new_states[[move$key]]$score <- move$score
+          } else new_states[[move$key]] <- move
+        }
+      }
+    }
+    message('Exploring ', length(new_states), ' new states')
+    states <- new_states
+  }
+  best_score
 }
 
 move_cost <-
@@ -30,18 +59,29 @@ distance <- matrix(
   dimnames = list(hallway = NULL,
                   room = paste0(rep(LETTERS[1:4], each = 2), 1:2)))
 
+distance2 <- cbind(distance[, 1:2], distance[, 2] + 1, distance[, 2] + 2,
+                   distance[, 3:4], distance[, 4] + 1, distance[, 4] + 2,
+                   distance[, 5:6], distance[, 6] + 1, distance[, 6] + 2,
+                   distance[, 7:8], distance[, 8] + 1, distance[, 8] + 2)
+colnames(distance2) <- paste0(rep(LETTERS[1:4], each = 4), 1:4)
+
 State <- R6::R6Class(
   'State',
   public = list(
     rooms = NULL,     # named vector, A1, A2, B1, B2, C1, C2, D1, D2
     hallways = NULL,  # vector of length 7. Unnamed but order is important.
     score = NULL,
+    finished = NULL,
+    key = NULL,
     initialize = function(rooms, hallways = NULL, score = 0) {
       self$rooms <- rooms
       self$hallways <- if(!is.null(hallways)) hallways else rep('.', 7)
       self$score <- score
+      self$finished <- all(self$hallways == '.') &&
+        all(self$rooms == rep(LETTERS[1:4], each = length(rooms) / 4))
+      self$key = paste(c(hallways, rooms), collapse = ',')
     },
-    move = function(from, to, from_hallway, score) { #TODO
+    move = function(from, to, from_hallway, score) {
       rooms <- self$rooms
       hallways <- self$hallways
       if (from_hallway) {
@@ -87,13 +127,16 @@ hallway_paths <- list(
 )
 
 valid_moves <- function(state) {
+  room_depth <- length(state$rooms) / 4
+  if (room_depth == 4)
+    distance <- distance2
   moves <- NULL
   for (i in seq_along(state$hallways)) {
     amphipod <- state$hallways[i]
     # If this spot is empty, nothing to move.
     if (amphipod == '.')
       next
-    dest <- paste0(amphipod, 1:2)
+    dest <- paste0(amphipod, seq_len(room_depth))
     # If destination room contains other, different amphipods.
     if (any(setdiff(LETTERS[1:4], amphipod) %in% state$rooms[dest]))
       next
@@ -104,6 +147,7 @@ valid_moves <- function(state) {
     # Move to the deepest available space in destination room.
     dest <- dest[max(which(state$rooms[dest] == '.'))]
     stopifnot(length(dest) > 0)
+    if (!dest %in% colnames(distance)) browser()
     score <- state$score + distance[i, dest] * move_cost[amphipod]
     moves <- c(moves, state$move(from = i,
                                  to = dest,
@@ -114,24 +158,21 @@ valid_moves <- function(state) {
     amphipod <- state$rooms[i]
     room_cell <- names(amphipod)
     room <- substring(room_cell, 1, 1)
+    pos_in_room <- as.integer(substring(room_cell, 2, 2))
     # If this spot is empty, nothing to move.
     if (amphipod == '.')
       next
     # Can't leave this room if our exit is blocked by another amphipod.
-    if (i %% 2 == 0 && state$rooms[i - 1] != '.')
+    if (pos_in_room > 1 && state$rooms[i - 1] != '.')
       next
-    # No point in leaving this room if we're at our destination at depth 2.
-    if (i %% 2 == 0 && amphipod == room)
-      next
-    # No point in leaving our destination except to let another amphipod out.
-    if (i %% 2 == 1 && amphipod == room &&
-        state$rooms[i + 1] == amphipod)
+    # No point in leaving destination room except to let other types out.
+    if (room == amphipod &&
+        all(state$rooms[paste0(room, pos_in_room:room_depth)] == amphipod))
       next
     # Which hallway tile should we move to?
     empty_hallways <- which(state$hallways == '.')
-    # browser()
     for (h in empty_hallways) {
-      if (all(hallway_paths[[h]][[room]] %in% empty_hallways)) {
+      if (all(hallway_paths[[h]][[room]] %in% empty_hallways)) { # not blocked
         score <- state$score + distance[h, room_cell] * move_cost[amphipod]
         moves <- c(moves, state$move(from = i,
                                      to = h,
--- inst/solution23.R
+++ inst/solution23.R
@@ -1,5 +1,12 @@
 library(adventofcode2021)
-input <- read_amphipod(input_file(23))
-start <- State$new(input$rooms, input$hallways)
-start
-valid <- valid_moves(start)
+# Part 1
+start <- read_amphipod(input_file(23))
+min_amphipod_energy(start) # ~ 120s
+# Part 2
+start2 <- start$clone()
+start2$rooms <- setNames(c(rbind(matrix(start$rooms, 2)[1, ],
+                                 c('D', 'C', 'B', 'A'),
+                                 c('D', 'B', 'A', 'C'),
+                                 matrix(start$rooms, 2)[2, ])),
+                         paste0(rep(LETTERS[1:4], each = 4), 1:4))
+min_amphipod_energy(start2) # ~ 135s
